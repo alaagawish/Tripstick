1d90be7a1239f1b75b511532f92b4abda243ecdc
Alaa Gawish <53160167+alaagawish@users.noreply.github.com>
2023-12-21 12:51:18 +0200

    handling close button in member card

--- Tripstick/ViewController.swift
+++ Tripstick/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var memberViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var cancelButton: UIImageView!
+    @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var exploreButton: UIButton!
     @IBOutlet weak var joinButton: UIButton!
     @IBOutlet weak var memberView: UIView!
     @IBOutlet weak var informationView: UIView!
     @IBOutlet weak var travelInspirationView: UIView!
+    @IBOutlet weak var parentViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var passangerImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -21,22 +26,34 @@ class ViewController: UIViewController {
         initUI()
     }
     
+    @IBAction func cancelMemberCard(_ sender: Any) {
+        
+        memberView.isHidden = true
+        passangerImage.isHidden = true
+        memberViewHeightConstraint.constant = 0
+        parentViewHeightConstraint.constant = 700
+        
+    }
     func initUI(){
-        joinButton.layer.cornerRadius = 15.0
+        joinButton.layer.cornerRadius = 20.0
+        searchTextField.layer.cornerRadius = 28.0
+        searchTextField.layer.borderWidth = 2.0
+        searchTextField.layer.masksToBounds = true
+        searchTextField.layer.borderColor = UIColor.black.cgColor
         exploreButton.layer.cornerRadius = 15.0
         memberView.layer.borderWidth = 2.0
         memberView.layer.borderColor = UIColor.black.cgColor
         
-        memberView.layer.cornerRadius = 15.0
-        memberView.layer.borderColor = UIColor.gray.cgColor
+        memberView.layer.cornerRadius = 20.0
+        memberView.layer.borderColor = UIColor.lightGray.cgColor
         informationView.layer.borderWidth = 2.0
         informationView.layer.borderColor = UIColor.black.cgColor
         
         informationView.layer.cornerRadius = 15.0
-        informationView.layer.borderColor = UIColor.gray.cgColor
+        informationView.layer.borderColor = UIColor.lightGray.cgColor
         travelInspirationView.layer.borderWidth = 2.0
         travelInspirationView.layer.borderColor = UIColor.black.cgColor
-        travelInspirationView.layer.borderColor = UIColor.gray.cgColor
+        travelInspirationView.layer.borderColor = UIColor.lightGray.cgColor
         travelInspirationView.layer.cornerRadius = 15.0
     }
     
