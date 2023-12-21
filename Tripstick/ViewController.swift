c713dc864bcdaae0c5836a4d3a0b978855df4597
Alaa Gawish <53160167+alaagawish@users.noreply.github.com>
2023-12-21 13:53:57 +0200

    update stack dimensions

--- Tripstick/ViewController.swift
+++ Tripstick/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var passangerImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
         
         initUI()
     }
